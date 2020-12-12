18a11c90cafc514aedb07ba65e763e9cebc68369
Paweł Pindel <pawel.pindel@netsharpdev.com>
2020-12-12 19:11:22 +0100

    Fix vnet template

--- Tydzien3/deploy.ps1
+++ Tydzien3/deploy.ps1
@@ -1,1 +1,19 @@
-Connect Az-Account
+﻿Connect-AzAccount
+
+$rg = 'rg-netsharp-learn'
+
+# Generate a SAS token
+$VNetLinkedTemplateUri = "https://raw.githubusercontent.com/netsharpdev/az-303-szkola-chmury/master/Tydzien3/Vnet.json"
+
+$VMLinkedTemplateUri = "https://raw.githubusercontent.com/netsharpdev/az-303-szkola-chmury/master/Tydzien3/vm-create-arm.json"
+
+# Deploy the template
+New-AzResourceGroupDeployment `
+  -Name 'DeployLinkedTemplate' `
+  -ResourceGroupName $rg `
+  -TemplateFile '.\tydzien3.json' `
+  -VnetTemplateUrl $VNetLinkedTemplateUri `
+  -VmTemplateUrl $VMLinkedTemplateUri `
+  -EnvironmentName 'dev' `
+  -AdminUserVm 'NetsharpAdmin' `
+  -AdminPasswordVm 'Netsharp123!'
